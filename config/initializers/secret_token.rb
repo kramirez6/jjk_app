c2df4e6ac376e0661dde9e5d7df595d28115a552
' <'>
2014-01-23 14:23:03 -0600

    Created the APP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JjkApp::Application.config.secret_key_base = 'c3b5a5e94bacfe7e57eb5e61700a1386961800ff120f0a02e8ddf88e40b60af6c56dd340f00581d21df17586c7d5f8d03d2d30a45d028c48e8e1328cf84fcc29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JjkApp::Application.config.session_store :cookie_store, key: '_jjk_app_session'
